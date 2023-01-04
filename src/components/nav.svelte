6ea248ebbee29b30c4dc33a0b397d935f80f97db
Andy Parinas <andy@budgetpetproducts.com.au>
2023-01-04 16:58:48 +1000

    using serverside fetch

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,4 +1,4 @@
 <nav class="layout">
-    <a prefetch href="/">My Site</a>
-    <a prefetch href="/about">About Page</a>
+    <a  href="/">My Site</a>
+    <a  href="/about">About Page</a>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,23 @@
+
 <script>
-  import { onMount } from 'svelte'
 
+    export let data;
 
-    let data;
-    onMount(async () => {
-        const res = await fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=7c1985cbce31441c853525ce93e10e33")
+    // onMount(async () => {
+    //     console.log(recipes)
+    //     // const res = await fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=7c1985cbce31441c853525ce93e10e33")
 
-        const data = await res.json()
-        console.log(data)
-    })
+    //     // const {results} = await res.json()
+    //     // data = results
+    // })
 </script>
 
 
-<h1>Welcome to SvelteKit Updated</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Food Recipes</h1>
+
+{#each data.recipes as item}
+    <div>
+        <h3>{item.title}</h3>
+        <img src="{item.image}" alt="{item.title}" >
+    </div>
+{/each}
